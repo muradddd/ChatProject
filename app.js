658815cbe095b1f921478e926be7b07dd5304932
muradddd <murad.rustemzade@gmail.com>
2020-06-15 17:36:18 +0400

    body-parser & post method

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
 // LISTENING TO SERVER ON PORT 3000
 app.listen(3000);
+
+// PARSING REQUEST TO JSON
+app.use(bodyParser.json());
 
 // ROUTES
 app.get('/', (req, res) => res.send('goodbye world'));
--- routes/threads.js
+++ routes/threads.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('threads'));
 
+router.post('/', (req, res) => console.log(req.body));
 
 
 module.exports = router;
