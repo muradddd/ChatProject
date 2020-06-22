47e46da9ee1c26f423f50775b784b44b820ea3d0
muradddd <murad.rustemzade@gmail.com>
2020-06-23 03:42:17 +0400

    thread model created

---
+++ models/Thread.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const TreadSchema = mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Threads', TreadSchema);
--- routes/threads.js
+++ routes/threads.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
+const Threads = require('../models/Thread');
 
 router.get('/', (req, res) => res.send('threads'));
 
-router.post('/', (req, res) => console.log(req.body));
+router.post('/', async (req, res) => {
+    const { user } = req.body;
+    const thread = new Threads({
+        user
+    });
+    try {
+        const savedThread = await thread.save();
+        res.json(savedThread);
+    } catch (error) {
+        res.json(error);
+    }
+
+});
 
 
 module.exports = router;
