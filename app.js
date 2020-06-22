ad69097008b96fbb0979c9958cd46fe86e79fbbf
muradddd <murad.rustemzade@gmail.com>
2020-06-23 03:41:30 +0400

    database configured

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 // LISTENING TO SERVER ON PORT 3000
 app.listen(3000);
@@ -14,3 +16,14 @@ const threadsRoute = require('./routes/threads');
 
 // ROUTES MIDDLEWARES
 app.use('/threads', threadsRoute);
+
+// CONNECT TO DATABASE
+mongoose.connect(process.env.DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: process.env.DB_NAME,
+    })
+    .catch(err => console.log(err));  // Error on initial connection
+
+mongoose.connection.on('error', err => console.log(err)); // Error after initial connection was established
